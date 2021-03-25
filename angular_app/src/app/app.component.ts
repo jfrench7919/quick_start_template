bb95be1dbaa2d14d228ddbb20dbc0c028f27035f
Joshua French <11663405+jfrench7919@users.noreply.github.com>
2021-03-25 15:59:30 -0400

    added servcie app and docker files

--- angular_app/src/app/app.component.ts
+++ angular_app/src/app/app.component.ts
@@ -1,10 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'app works!';
+
+  // Link to our api, pointing to localhost
+  API = 'http://localhost:3000';
+
+  // Declare empty list of people
+  people: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  // Angular 2 Life Cycle event when component has been initialized
+  ngOnInit() {
+    this.getAllPeople();
+  }
+
+  // Add one person to the API
+  addPerson(name: any, age: any) {
+    this.http.post(`${this.API}/users`, { name, age })
+      .subscribe((data: any) => {
+        this.getAllPeople();
+      }, (error: any) => { console.log(error); });
+  }
+
+  // Get all users from the API
+  getAllPeople() {
+    this.http.get(`${this.API}/users`)
+      .subscribe((people: any) => {
+        console.log(people)
+        this.people = people
+      }, (error: any) => { console.log(error); });
+  }
 }
